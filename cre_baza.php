6897a0e94281df9c4bfcf818ba2b59a05b23bf14
SSFIR <imagine.ssfi@gmail.com>
2024-07-17 20:16:15 +0300

    test1
    test1

--- cre_baza.php
+++
@@ -1,9 +0,0 @@
-<?php
-$msql=mysql_connect("localhost","root","");
-if(!$msql)
-{echo "Nu s-a realizat conectarea la MySQL";
-exit;}
-$rezultat=mysql_query("CREATE DATABASE testest");
-if(!$rezultat)
-	echo "Nu s-a realizat crearea datei de baze";
-?>
